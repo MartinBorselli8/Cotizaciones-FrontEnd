02994b5fe314be157076d392a1d8a07ed50a4f9c
LucasLahournere <lahournerelucas@gmail.com>
2022-06-28 21:39:22 -0300

    Se agrego la vista cliente

---
+++ Cotizaciones-FrontEnd/Controllers/ClientController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cotizaciones_FrontEnd.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
