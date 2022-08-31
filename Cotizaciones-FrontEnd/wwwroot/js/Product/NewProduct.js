3cb2bed41faea5b65cfffd338d6c3d618ec9530f
Nicolás Priotti <nico.priotti8@gmail.com>
2022-08-31 15:05:15 -0300

    Se implemento la vista de producto.

---
+++ Cotizaciones-FrontEnd/wwwroot/js/Product/NewProduct.js
@@ -0,0 +1,24 @@
+﻿
+function newProduct() {
+    const Description = $("#Description").val()
+    const UnitPrice = $("#UnitPrice").val()
+
+
+    $.ajax({
+        url: 'https://localhost:44379/api/Product?Description=' + Description + '&UnitPrice=' + UnitPrice,
+        type: "POST",
+        success: function (response) {
+            
+            if (response.fueCreado == true) {
+                Swal.fire(
+                    '¡Genial!',
+                    'El producto se guardó con éxito.',
+                    'success'
+                )
+                setTimeout(function () { window.location.href = "/Products/IndexProducts"; }, 5000);
+            }
+        }
+
+
+    })
+}
